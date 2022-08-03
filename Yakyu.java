fcbc731d340bb034ca6646fb8746ab4ebda54ebb
hatotenpura <108779035+hatotenpura@users.noreply.github.com>
2022-08-03 19:33:43 +0900

    Update Yakyu.java

--- Yakyu.java
+++ Yakyu.java
@@ -5,33 +5,42 @@ public class Yakyu {
 		
 		Random rand = new Random();
 		
-		
+		//合計点の変数は事前に初期化しておく
 		int scoreA = 0;
 		int scoreB = 0;
 		
+		//↓上のやつ
+		for (int a = 1; a < 10; a++) {
+			System.out.print(a + " ");
+		}
+		System.out.println("計");
+		
+		
 		//↓Aチームの点数
 		for (int a = 1; a < 10; a++) {
-			//ランダムな点数が出るscoreを作成する forの外にすると数値が固定されるっぽいので中で作成
+			//ランダムな点数が出る変数score　　for文の外だと数値が固定されるので中で作成する
 			int score = rand.nextInt(10);
-			//表示する
+			//表示
 			System.out.print(score + " ");
 			
-			//最終的な合計が欲しいのでscoreAに足していく
+			//合計の点数が欲しいのでscoreAに足していく
 			scoreA += score;
 		}
 		//for文の外でscoreAを出力　合計が出る（はず）
 		System.out.println(scoreA);
 		
+		
+		
 		//Bチームの点数 ループは8回まで
 		for (int a = 1; a < 9; a++) {
 			int score = rand.nextInt(10);
-			//表示する
+			//表示
 			System.out.print(score + " ");
 			//scoreBに足していく
 			scoreB += score;
 		}
 		
-		//BのスコアがAより高いなら×を出力、Aより低いならもう一度ランダムを回す
+		//9回目　BのスコアがA以上なら× を出力、Aより低いならもう一度ランダムで回して合計に足す
 		if (scoreA <= scoreB) {
 			System.out.print("× ");
 		} else {
