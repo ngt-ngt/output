8dbd81a7f974f7b3459515bb969ff0e614cc537a
hatotenpura <108779035+hatotenpura@users.noreply.github.com>
2022-09-04 12:21:27 +0900

    Add files via upload

---
+++ rensyu/Main0904.java
@@ -0,0 +1,24 @@
+package rensyu;
+import java.util.Random;
+
+public class Main0904 {
+
+	public static void main(String[] args) {
+		// チワワとポメラニアンのキメラを作ろう!（改）
+		
+		Random rnd = new Random();
+		int[] word = new int[7];
+		int r = rnd.nextInt(5)+2; // 合計文字数はランダム　最低でも2文字
+		
+		for (int i = 0; i <= r; i++) {
+			word[i] = rnd.nextInt(7);
+		}		
+		
+		for (int i = 0; i <= r; i++) { 
+		char[] dog = {'チ','ワ','ポ','メ','ラ','ニ','ア','ン'};
+		System.out.print(dog[word[i]]);
+		}
+
+	}
+
+}
